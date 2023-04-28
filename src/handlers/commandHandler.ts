13fe6e715b9ae8231ce5829f78fe6c8f72181dce
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-04-28 11:21:44 +0200

    Begin Work

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -17,11 +17,31 @@ module.exports = async (client: Client) => {
         body.push(command.data.toJSON());
     });
 
+    const bodyRules = [];
+
+    bodyRules.push({
+        membership_screening:{
+            version: "2021-01-09T12:09:02.040000+00:00",
+            form_fields: [
+                {
+                    field_type: "TERMS",
+                    label: "Read and agree to the server rules",
+                    values: [
+                        "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.",
+                        "No spam or self-promotion (server invites, advertisements, etc) without permission from a staff member. This includes DMing fellow members."
+                    ],
+                    required: true
+                }
+            ],
+            description: "Welcome to this cool server!"
+        }
+    })
 
     const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     try {
         await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, GUILD_ID), { body: body });
+        await rest.put(Routes.guild(GUILD_ID), { body: bodyRules });
         console.log('Successfully reloaded application (/) commands.');
     }
     catch (error) {
