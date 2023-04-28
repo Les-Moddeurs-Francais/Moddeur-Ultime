079a5532774835d25cd168896699eb2cf53fc1a3
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-04-28 12:21:41 +0200

    Begin Work

---
+++ src/slashCommands/message.ts
@@ -0,0 +1,24 @@
+import {SlashCommandBuilder} from "discord.js"
+import {SlashCommand} from "../types";
+
+export const command: SlashCommand = {
+    name: 'message',
+    data: new SlashCommandBuilder()
+        .setName("message")
+        .setDescription("Retourne la latence du bot")
+        .addStringOption((option) => {
+            return option
+                .setName("contenu")
+                .setDescription("Contenu du message à envoyer")
+                .setRequired(true)
+        }),
+    execute: async (interaction) => {
+        await interaction.channel.send({
+            content: `${interaction.options.getString("contenu")}`
+        })
+        await interaction.reply({
+            content: `Le message a été envoyé !`,
+            ephemeral: true
+        })
+    }
+}
