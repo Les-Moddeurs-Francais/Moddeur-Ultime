80cc8808ef152009901c7567b477cf4fe2806651
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-04-28 12:15:17 +0200

    WIP

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -20,28 +20,26 @@ module.exports = async (client: Client) => {
     const bodyRules = [];
 
     bodyRules.push({
-        membership_screening:{
-            version: "2021-01-09T12:09:02.040000+00:00",
-            form_fields: [
-                {
-                    field_type: "TERMS",
-                    label: "Read and agree to the server rules",
-                    values: [
-                        "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.",
-                        "No spam or self-promotion (server invites, advertisements, etc) without permission from a staff member. This includes DMing fellow members."
-                    ],
-                    required: true
-                }
-            ],
-            description: "Welcome to this cool server!"
-        }
-    })
+        enabled: true,
+        form_fields: [
+            {
+                field_type: "TERMS",
+                label: "Read and agree to the server rules",
+                values: [
+                    "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.",
+                    "No spam or self-promotion (server invites, advertisements, etc) without permission from a staff member. This includes DMing fellow members."
+                ],
+                required: true
+            }
+        ],
+        description: "Test Server"
+    });
 
     const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     try {
         await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, GUILD_ID), { body: body });
-        await rest.put(Routes.guild(GUILD_ID), { body: bodyRules });
+        await rest.patch(`/guilds/${GUILD_ID}/member-verification`, { body: bodyRules });
         console.log('Successfully reloaded application (/) commands.');
     }
     catch (error) {
