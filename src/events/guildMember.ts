6d88e2562598bfbe3a397e81288484a50354815e
Mysterious_Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2023-10-08 13:18:51 +0200

    Upgrade to Node js 20.8.0 (#24)
    * Update to NodeJS 20.8.0
    
    * Ooops
    
    * Ignore dev.env

--- src/events/guildMember.ts
+++ src/events/guildMember.ts
@@ -1,14 +1,13 @@
 import {BotEvent} from "../types";
 import {Events, GuildMember, TextChannel} from "discord.js";
-import {APPROBATION_RULES_CHANNEL, NPE_ROLE} from "../utils/id";
 
 const event: BotEvent = {
     name: Events.GuildMemberUpdate,
     once: false,
     execute(oldMember: GuildMember, newMember: GuildMember) {
         if(newMember.pending === false && oldMember.pending === true){
-            const npe = newMember.guild.roles.cache.get(NPE_ROLE);
-            const channel = oldMember.client.channels.cache.get(APPROBATION_RULES_CHANNEL) as TextChannel;
+            const npe = newMember.guild.roles.cache.get(process.env.NPE_ROLE);
+            const channel = oldMember.client.channels.cache.get(process.env.APPROBATION_RULES_CHANNEL) as TextChannel;
             newMember.roles.add(npe);
             channel.send(`<@${newMember.id}> a accepté les règles et en assumera les conséquences !`);
         }
